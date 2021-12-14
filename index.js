753f5cd98f9f523497224d633f0e7178c1d92820
Russkazkin <russkazkin@gmail.com>
2021-12-14 17:50:20 +0300

    Start http server

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Primary file for the API
+ */
+
+//Dependencies
+const http = require('http');
+
+const server = http.createServer(((req, res) => {
+    res.end('Hello World!\n');
+}));
+
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000');
+});

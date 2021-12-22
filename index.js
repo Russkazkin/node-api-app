d13132b7db9464d7d713ed786d7ae35059ab919c
Russkazkin <russkazkin@gmail.com>
2021-12-22 19:16:30 +0300

    Add request payload parsing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 //Dependencies
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer(((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -13,8 +14,14 @@ const server = http.createServer(((req, res) => {
     const method = req.method.toUpperCase();
     const queryStringObject = parsedUrl.query;
     const headersObject = req.headers;
-    res.end('Hello World!\n');
-    console.log(headersObject, 'headers');
+    const decoder = new StringDecoder('utf-8');
+    let payload = '';
+    req.on('data', data => payload += decoder.write(data));
+    req.on('end', () => {
+        payload += decoder.end()
+        res.end('Hello World!\n');
+        console.log(payload, 'payload');
+    });
 }));
 
 server.listen(3000, () => {
